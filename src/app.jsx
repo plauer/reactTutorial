c9cf5c72eedd9bf49df16944303d33e3c47e05bc
plauer <plauer411@gmail.com>
2014-12-11 15:16:50 -0700

    box changes value when clicked

--- src/app.jsx
+++ src/app.jsx
@@ -13,22 +13,31 @@ var Box = React.createClass({
       value: 'X'
     }
   },
-  componentWillMount: function(){
-    var _this = this;
-    this.timer = setInterval(function(){
-      var oldValue = _this.state.value;
-      var newValue = oldValue === 'X' ? 'O': 'X';
-      _this.setState({
-        value: newValue
-      });
-    }, 300);
+  handleClick: function() {
+    var oldValue = this.state.value;
+    var newValue = oldValue === 'X' ? 'O': 'X';
+    this.setState({
+      value: newValue
+    });
   },
-  componentWillUnmount: function(){
-    clearInterval(this.timer);
-  },
+
+
+  // componentWillMount: function(){
+  //   var _this = this;
+  //   this.timer = setInterval(function(){
+  //     var oldValue = _this.state.value;
+  //     var newValue = oldValue === 'X' ? 'O': 'X';
+  //     _this.setState({
+  //       value: newValue
+  //     });
+  //   }, 300);
+  // },
+  // componentWillUnmount: function(){
+  //   clearInterval(this.timer);
+  // },
   render: function(){
     return (
-      <button style={this.props.style}>{this.state.value}</button>
+      <button style={this.props.style} onClick={this.handleClick}>{this.state.value}</button>
     )
   }
 });
